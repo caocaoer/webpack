e8445fca66f18ddb314f9c368acaa109799f2d31
caocaoer <caocaoer1019@163.com>
2016-09-27 10:46:17 +0800

    add

---
+++ demo/03/index.jsx
@@ -0,0 +1,9 @@
+var x = 1;
+function foo(x, y = function() { x = 2; }) {
+  var x = 3;
+  y();
+  console.log(x);
+}
+
+foo();
+
---
+++ demo/03/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: './index.jsx',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js[x]?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader?presets[]=es2015&presets[]=react'
+        }]
+    }
+};
