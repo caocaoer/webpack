a7b820d2a9c4c00dee0d01b1dd89627f3cf2e261
caocaoer <caocaoer1019@163.com>
2016-09-28 09:37:08 +0800

    add

---
+++ demo/08/webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new uglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            title: 'webpack-demo',
+            filename: 'index.html'
+        }),
+        new OpenBrowserPlugin({
+            url: 'http://localhost:8080'
+        })
+    ]
+};
